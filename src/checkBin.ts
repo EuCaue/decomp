41fe49c3d3d8b5a17fd6ab698408b9b90836c8da
EuCaue <caueadriano.castro@gmail.com>
2024-01-22 21:48:07 -0300

    chore: remove todo

--- src/checkBin.ts
+++ src/checkBin.ts
@@ -5,7 +5,6 @@ export default function checkBin(program: string): boolean {
   let isAvailableOnSystem = false;
 
   try {
-    //  TODO: this need to be tested on windows
     exec(`command -v ${program}`);
     isAvailableOnSystem = true;
     console.info(`${program} it's available on your path.`);
