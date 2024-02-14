a2a9eefe214350606ce00f2ae2d9c7f480cff7ce
EuCaue <caueadriano.castro@gmail.com>
2024-02-14 10:25:46 -0300

    feat(index.ts): check if the file exists

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import { extname } from "path";
 import { exit } from "process";
 import { files, options } from "@/argParser";
+import { constants, accessSync } from "fs";
 import extractFile from "@/extractFile";
 
 const hasOneFile: boolean = files.length === 1;
@@ -12,12 +13,28 @@ function main(file: string): void {
   extractFile(file, fileType, options);
 }
 
+function checkFileExists(path: string): void {
+  accessSync(path, constants.F_OK);
+}
+
+//  TODO: refactor this code
 if (hasOneFile) {
   const file: string = files[0];
-  main(file);
+  try {
+    checkFileExists(file);
+    main(file);
+  } catch (e) {
+    console.error("File does not exist!");
+    exit(1);
+  }
 } else if (hasTwoOrMoreFiles) {
   files.forEach((file) => {
-    main(file);
+    try {
+      checkFileExists(file);
+      main(file);
+    } catch (e) {
+      console.error("File does not exist!");
+    }
   });
 } else {
   console.error("No file(s) to extract!");
