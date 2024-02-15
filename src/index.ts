d0bc2433ea75ccc400a20bdb533dbdb90a50a1d8
EuCaue <caueadriano.castro@gmail.com>
2024-02-15 17:56:42 -0300

    refactor(index.ts): refactor main function

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
 import { extname } from "path";
-import { exit } from "process";
 import { files, options } from "@/argParser";
 import { constants, accessSync } from "fs";
 import extractFile from "@/extractFile";
-
-const hasOneFile: boolean = files.length === 1;
-const hasTwoOrMoreFiles: boolean = files.length >= 2;
 
 function main(file: string): void {
   const fileType = extname(file);
@@ -17,26 +13,11 @@ function checkFileExists(path: string): void {
   accessSync(path, constants.F_OK);
 }
 
-//  TODO: refactor this code
-if (hasOneFile) {
-  const file: string = files[0];
+files.forEach((file) => {
   try {
     checkFileExists(file);
     main(file);
   } catch (e) {
     console.error("File does not exist!");
-    exit(1);
   }
-} else if (hasTwoOrMoreFiles) {
-  files.forEach((file) => {
-    try {
-      checkFileExists(file);
-      main(file);
-    } catch (e) {
-      console.error("File does not exist!");
-    }
-  });
-} else {
-  console.error("No file(s) to extract!");
-  exit(1);
-}
+});
