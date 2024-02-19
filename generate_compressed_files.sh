fe3ed0393d37ddb34b610de1388c5af061a8e70b
EuCaue <caueadriano.castro@gmail.com>
2024-02-19 19:23:41 -0300

    test: improve script to generate test archives.

--- generate_compressed_files.sh
+++ generate_compressed_files.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Supported archive formats
 formats=("tar.bz2" "tb2" "tbz" "tbz2" "tz2" "tar.gz" "tgz" "taz" "tlz" "tar.lz" "tar.lzma" "tar.lzo" "tar.xz" "txz" "tar.Z" "tZ" "taZ" "tar.zst" "zip" "7z" "rar")
 DEST_FOLDER="./test_archive"
-
 
 echo """
 
@@ -26,65 +25,69 @@ echo """
 
 mkdir -p "$DEST_FOLDER"
 
-cd "$DEST_FOLDER"
+cd "$DEST_FOLDER" || exit
+
+check_bin() {
+	if ! command -v "$1" >/dev/null 2>&1; then
+		echo "$1 not found in path! Skipping generate $1 archive."
+		return 1
+	elif [[ -n "$2" ]]; then
+		eval "$2" >/dev/null 2>&1
+		return 0
+	fi
+}
 
 # main loop
 for format in "${formats[@]}"; do
-    if [[ "$format" == "zip" ]]; then
-        dest_name="Archive{zip}.zip"
-    elif [[ "$format" == "7z" ]]; then
-        dest_name="Archive{7z}.7z"
-    else
-        dest_name="Archive{$format}.$format"
-    fi
-    
-    echo "$dest_name"
-    demo_file="Demo for $format.txt"
-    echo "$demo_file"
-    echo "You managed to successfully extract a $format!" > "$demo_file"
-    
-    case "$format" in
-        "rar")
-            rar a "$dest_name" "$demo_file" > /dev/null
-            ;;
-        "zip")
-            zip -qj "$dest_name" "$demo_file"
-            ;;
-        "7z")
-            7z a "$dest_name" "$demo_file" > /dev/null
-            ;;
-        "tar.bz2"|"tb2"|"tbz"|"tbz2"|"tz2")
-            tar -cjf "$dest_name" "$demo_file"
-            ;;
-        "tar.gz"|"tgz"|"taz")
-            tar -czf "$dest_name" "$demo_file"
-            ;;
-        "tar.lz")
-            tar --lzip -cf "$dest_name" "$demo_file"
-            ;;
-        "tar.lzma"|"tlz")
-            tar --lzma -cf "$dest_name" "$demo_file"
-            ;;
-        "tar.lzo")
-            tar --lzop -cf "$dest_name" "$demo_file"
-            ;;
-        "tar.xz"|"txz")
-            tar -cJf "$dest_name" "$demo_file"
-            ;;
-        "tar.Z"|"tZ"|"taZ")
-            tar -cZf "$dest_name" "$demo_file"
-            ;;
-        "tar.zst")
-            tar --zstd -cf "$dest_name" "$demo_file"
-            ;;
-        *)
-            echo "$format not supported! Skpping..."
-            ;;
-    esac
-    
-    rm "$demo_file"
-    echo "Archive created: $format"
+	dest_name="Archive{$format}.$format"
+
+	echo "$dest_name" >/dev/null 2>&1
+	demo_file="Demo for $format.txt"
+	echo "$demo_file" >/dev/null 2>&1
+	echo "You managed to successfully extract a $format!" >"$demo_file"
+
+	#  TODO: check if the binary it's available on the path.
+	case "$format" in
+	"rar")
+		check_bin "rar" "rar a \"$dest_name\" \"$demo_file\""
+		;;
+	"zip")
+		check_bin "zip" "zip -qj \"$dest_name\" \"$demo_file\""
+		;;
+	"7z")
+		check_bin "7z" "7z a \"$dest_name\" \"$demo_file\""
+		;;
+	"tar.bz2" | "tb2" | "tbz" | "tbz2" | "tz2")
+		(check_bin "bzip2" && check_bin "tar" "tar -cjf \"$dest_name\" \"$demo_file\"") || continue
+		;;
+	"tar.gz" | "tgz" | "taz")
+		check_bin "tar" "tar -czf \"$dest_name\" \"$demo_file\""
+		;;
+	"tar.lz")
+		(check_bin "lzip" && check_bin "tar" "tar --lzip -cf \"$dest_name\" \"$demo_file\"") || continue
+		;;
+	"tar.lzma" | "tlz")
+		(check_bin "lzma" && check_bin "tar" "tar --lzma -cf \"$dest_name\" \"$demo_file\"") || continue
+		;;
+	"tar.lzo")
+		(check_bin "lzop" && check_bin "tar" "tar --lzop -cf \"$dest_name\" \"$demo_file\"") || continue
+		;;
+	"tar.xz" | "txz")
+		tar -cJf "$dest_name" "$demo_file"
+		;;
+	"tar.Z" | "tZ" | "taZ")
+		tar -cZf "$dest_name" "$demo_file"
+		;;
+	"tar.zst")
+		(check_bin "zstd" && check_bin "tar" "tar --zstd -cf \"$dest_name\" \"$demo_file\"") || continue
+		;;
+	*)
+		echo "$format not supported! Skpping..."
+		;;
+	esac
+	echo "Archive created: $format"
 done
 
-cd ../
-echo "Done!\n"
+rm ./*.txt &&
+	cd ../
+printf "Done!\n"
