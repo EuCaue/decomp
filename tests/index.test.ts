63ce637afa5bcf8f579c9e7261a6715068e372b2
EuCaue <caueadriano.castro@gmail.com>
2024-02-19 19:23:14 -0300

    test: refactor and improve test for extractFile

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,24 +1,60 @@
 /* eslint-disable import/no-unresolved */
 import { describe, expect, it } from "bun:test";
-import { readdir } from "fs";
+import { access, readdir } from "fs/promises";
 import { extname, resolve } from "path";
 import extractFile from "../src/extractFile";
 
-const TESTFOLDER = resolve("./test_archive");
+const TESTDIR = resolve("./test_archive");
+const OUTPUTDIR = `${TESTDIR}/extracted`;
 
-describe("Extract File", () => {
-  readdir(TESTFOLDER, (_, files) => {
-    files.forEach(async (file) => {
-      const fileType = extname(file);
-      const fileParsed = resolve(`${TESTFOLDER}/${file}`);
-      it(`should extract the ${fileType} file into the extracted folder`, async () => {
-        if (fileType) {
-          const result = await extractFile(fileParsed, fileType, {
-            outdir: `${TESTFOLDER}/extracted`
-          });
-          expect(result).toBeTrue();
-        }
-      });
+async function checkFileExists(path: string) {
+  try {
+    await access(path);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+function getPrefixBeforeFirstDot(str: string): string {
+  const regex = /.*?}/;
+  const result = str.match(regex)![0];
+  return result;
+}
+
+async function processFile(file: string) {
+  // eslint-disable-next-line no-async-promise-executor
+  return new Promise(async (resolvePromise) => {
+    const fileType = extname(file);
+    const filePathResolved = resolve(`${TESTDIR}/${file}`);
+
+    const result = await extractFile(filePathResolved, fileType, {
+      outdir: OUTPUTDIR
     });
+    resolvePromise(result);
   });
+}
+
+describe("Extract files", async () => {
+  try {
+    const files = await readdir(TESTDIR);
+    const filesWithExtensions = files.filter(
+      (file) => extname(file).length > 0
+    );
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const file of filesWithExtensions) {
+      it(`should extract the ${file} file into the ${getPrefixBeforeFirstDot(file)} folder`, async () => {
+        const resultExtractedFile = await processFile(file);
+        expect(resultExtractedFile).toBeTrue();
+        const extractedFilePath = `${OUTPUTDIR}/${getPrefixBeforeFirstDot(file)}`;
+        const isFileExtracted = await checkFileExists(extractedFilePath);
+        expect(isFileExtracted).toBeTrue();
+      });
+    }
+  } catch (error) {
+    console.error(
+      "Test files or folder does not exist, please run the script from the root of the project and try again."
+    );
+  }
 });
